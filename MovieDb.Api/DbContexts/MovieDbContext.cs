1374da07156adeb31ce3e5716213b13bd0d756bb
Dan Coupar <dan.coupar@gmail.com>
2025-03-07 22:27:51 +0000

    Add EF Core and SQLite database and seed with data from csv.

---
+++ MovieDb.Api/DbContexts/MovieDbContext.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper;
+using Microsoft.EntityFrameworkCore;
+using MovieDb.Api.Entities;
+using System.Globalization;
+
+namespace MovieDb.Api.DbContexts
+{
+	public class MovieDbContext : DbContext
+	{
+		public DbSet<Movie> Movies { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			options
+				.UseSqlite("Data Source=Data/movies.db")
+				.UseSeeding((context, _) =>
+				{
+					DbSet<Movie> moviesDbSet = context.Set<Movie>();
+
+					if (moviesDbSet.Any())
+					{
+						return;
+					}
+
+					using var reader = new StreamReader("Assets/mymoviedb.csv");
+					using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+					csv.Context.RegisterClassMap<CsvMap>();
+					moviesDbSet.AddRange(csv.GetRecords<Movie>());
+					context.SaveChanges();
+				}
+			);
+		}
+	}
+}
