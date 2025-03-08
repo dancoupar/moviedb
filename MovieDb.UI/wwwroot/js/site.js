7f5daa4ee1e0fdcafa3dba9b95aa25c749ab0ef7
Dan Coupar <dan.coupar@gmail.com>
2025-03-08 15:48:57 +0000

    Implement paging in UI.

--- MovieDb.UI/wwwroot/js/site.js
+++ MovieDb.UI/wwwroot/js/site.js
@@ -5,20 +5,24 @@
 
 window.onload = function () {
     document.getElementById("searchButton").onclick = function () {
-        const titleContains = document.getElementById("titleSearchInput").value;
-        const maxResults = document.getElementById("maxResultsSelect").value;        
-        fetch(`https://localhost:7177/movies?titleContains=${titleContains}&maxNumberOfResults=${maxResults}&pageSize=100&pageNumber=1`)
-            .then(response => response.json())
-            .then(data => renderSearchResults(data.content))
-            .catch(error => console.error('Error during search: ', error));
+        performSearch(1);
 	};
+}
+
+function performSearch(pageNumber) {
+    const titleContains = document.getElementById("titleSearchInput").value;
+    const maxResults = document.getElementById("maxResultsSelect").value;
+    fetch(`https://localhost:7177/movies?titleContains=${titleContains}&maxNumberOfResults=${maxResults}&pageSize=20&pageNumber=${pageNumber}`)
+        .then(response => response.json())
+        .then(data => renderSearchResults(data))
+        .catch(error => console.error('Error during search: ', error));
 }
 
 function renderSearchResults(data) {
     const tableBody = document.getElementById("searchResults");
     tableBody.innerHTML = "";
 
-    data.forEach(item => {
+    data.content.forEach(item => {
         const row = document.createElement("tr");
 
         const titleCell = document.createElement("td");
@@ -35,4 +39,21 @@ function renderSearchResults(data) {
 
         tableBody.appendChild(row);
     });
+
+    renderPagingLinks(data);
+}
+
+function renderPagingLinks(data) {
+    const numberOfPages = Math.ceil(data.totalElements / data.pageSize);
+    
+    const pagingLinks = document.getElementById("pagingLinks");
+    pagingLinks.innerHTML = "";
+
+    for (let i = 1; i < numberOfPages + 1; i++) {
+        let pageLink = document.createElement("a");
+        pageLink.textContent = i.toString();
+        pageLink.href = "#";
+        pageLink.onclick = function () { performSearch(i); };
+        pagingLinks.append(pageLink);
+    }
 }
